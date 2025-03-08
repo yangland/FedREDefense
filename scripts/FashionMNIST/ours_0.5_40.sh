4afd176e78f9b0fc8f9e701baac68f93e62be79b
liyang <yangland@me.com>
2025-03-08 10:15:19 +0800

    added for ours sh files

--- scripts/FashionMNIST/ours_0.5_40.sh
+++ scripts/FashionMNIST/ours_0.5_40.sh
@@ -1,18 +1,18 @@
 cmdargs=$1
 # aggregation_mode: "FedAVG","median", "NormBound","trmean","krum","flame", "RLR"
 # attack_method: "AOP", "UAM"
-export CUDA_VISIBLE_DEVICES='6'
+export CUDA_VISIBLE_DEVICES='0'
 hyperparameters04='[{
     "random_seed" : [4],
     "dataset" : ["fmnist"],
-    "models" : [{"ConvNet" : 10}],
+    "models" : [{"ConvNet" : 100}],
 
     "attack_rate" :  [ 0, 0.25, 0.4],
     "attack_method": ["untargeted_cos"],
     "participation_rate" : [1],
 
     "alpha" : [0.5],
-    "communication_rounds" : [100],
+    "communication_rounds" : [500],
     "local_epochs" : [1],
     "mali_local_epochs": [5],
     "batch_size" : [32],
